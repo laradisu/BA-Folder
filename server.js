fed5a7d2c768ad4ebdafa8bbf15c9c61f5427558
laradisu <32111302+laradisu@users.noreply.github.com>
2019-09-03 21:44:21 -0400

    passes 3 tests

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ var listingData, server;
 
 var requestHandler = function(request, response) {
   var parsedUrl = url.parse(request.url);
+  console.log(request.url);
+  if(request.method == 'GET' && request.url == '/listings')  {
+    //console.log("thisworks");
+    response.end(JSON.stringify(listingData));
+  }
+  else {
+    response.statusCode = 404;
+    //response.statusMessage = ;3
+    response.end('Bad gateway error');
+  }
 
   /*
     Your request handler should send listingData in the JSON format as a response if a GET request 
@@ -38,14 +48,28 @@ fs.readFile('listings.json', 'utf8', function(err, data) {
    */
 
     //Check for errors
-  
+    if (err) {
+      throw err;
+      console.log(data);
+    }
+    try {
+        listingData = JSON.parse(data)
+      }
+    catch(err) {
+        console.error(err)
+      }
 
    //Save the sate in the listingData variable already defined
-  
+   //??
 
   //Creates the server
-  
-  //Start the server
+  var server = http.createServer(requestHandler);
 
+  //Start the server
+  // listening for requests on port 8080 - go to your browser and paste in http://127.0.0.1:8080
+  server.listen(port, function() {
+    //once the server is listening, this callback function is executed
+    console.log('Server listening on: http://127.0.0.1:' + port);
+  });
 
 });
